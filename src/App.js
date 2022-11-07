8ea63b39c5cb698f9ec0c4f8165285b6e6894ff0
atulloch <50500011+stmblueprint@users.noreply.github.com>
2022-11-07 08:58:14 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ export default function App (){
         <>
 
      <div className="body-container">
-     <Leva hidden/>
+     <Leva display/>
 
         {/* Header with green background */}
           
