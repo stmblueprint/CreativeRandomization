593e40a0a904506b23085adeaa357e3907829820
atulloch <50500011+stmblueprint@users.noreply.github.com>
2022-11-07 16:07:00 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -238,7 +238,7 @@ export default function App (){
            </Canvas>
 
           </div>
-          <About/>
+            {/* <About/> */}
           {/* <div><Footer/></div> */}
           </div>
         </>
